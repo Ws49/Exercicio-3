3aaece41e2630164bf7176240ecf24204cbcf29f
Ws49 <senhaisaque@gmail.com>
2026-03-31 19:56:09 -0300

    front-end_v1.0.2

--- src/main/java/App/Application.java
+++ src/main/java/App/Application.java
@@ -16,10 +16,10 @@ public class Application {
 		setConfigsInit();
 		
 		get("/cars", (request, response) -> controllApp.getAllCars(request,response));
-		post("/cars", (request, response) -> controllApp.addCar(request, response),Controll.getHandleEngine());
 		get("/car/:id", (request, response) -> controllApp.getCar(request, response), Controll.getHandleEngine());
-		get("/car/update/:id", (request, response) -> controllApp.updateCar(request, response), Controll.getHandleEngine());
-		get("/car/delete/:id", (request, response) -> controllApp.deleteCar(request, response), Controll.getHandleEngine());
+		post("/cars", (request, response) -> controllApp.addCar(request, response),Controll.getHandleEngine());
+		post("/car/update/:id", (request, response) -> controllApp.updateCar(request, response), Controll.getHandleEngine());
+		post("/car/delete/:id", (request, response) -> controllApp.deleteCar(request, response), Controll.getHandleEngine());
 		
 	}
 	
